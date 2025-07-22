dbbc44baa2b258e17555ba90b478c3fb5afea75d
MAC <mac@MACs-MacBook-Pro.local>
2025-07-22 15:48:45 +0100

    Fix: updated auth.js to load service account

--- auth.js
+++ auth.js
@@ -1,9 +1,14 @@
 import { GoogleAuth } from 'google-auth-library';
 
 export async function authorize() {
+  const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT);
+
   const auth = new GoogleAuth({
-    keyFile: './service-account.json',
+    credentials: serviceAccount,
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
   });
+
   return await auth.getClient();
 }
+
+cat auth.js
